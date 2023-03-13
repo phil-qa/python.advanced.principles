ad65bf001e87ac7cb16389195fdf7067aa5b5fde
phil-qa <104486392+phil-qa@users.noreply.github.com>
2023-03-13 21:23:10 +0000

    Notebooks for 05 and 06

---
+++ AdvPyEG01.py
@@ -0,0 +1,68 @@
+source = ['zero', 'wun', 'two', 'tree', 'fower', 'fife', 'six', 'seven', 'ait', 'niner', 'alpha', 'bravo', 'charlie',
+          'delta', 'echo', 'foxtrot', 'golf', 'hotel', 'india', 'juliet', 'kilo', 'lima', 'mike', 'november', 'oscar',
+          'papa', 'quebec', 'romeo', 'sierra', 'tango', 'uniform', 'victor', 'whisky', 'xray', 'yankee', 'zulu']
+
+import string  # delimit numerics
+
+
+def merge(dictionary1, dictionary2):
+    '''
+    Merge two dictionaries together
+    :param dictionary1: dictionary
+    :param dictionary2: dictionary
+    :return: dictionary
+    '''
+    return (dictionary2.update(dictionary1))
+
+
+def delimit_types(data_to_delimit, delimit_point):
+    '''
+    separate the phonetic types into numeric and alphabetic
+    :param data_to_delimit:source phonetic alphabet
+    :param delimit_point: string value of list split
+    :return:
+    '''
+    index_of_alphabet = data_to_delimit.index(delimit_point)
+    numerics = data_to_delimit[0:index_of_alphabet]
+    alphabet = data_to_delimit[index_of_alphabet:len(data_to_delimit)]
+    return {"numerics": numerics, "alphabet": alphabet}
+
+
+def develop_key_lists():
+    '''
+    develop two data structures for numbers and letters
+    :return: dictionary of both
+    '''
+    numbers = list(string.digits)
+    letters = list(string.ascii_uppercase)
+    return {"numbers": numbers, "letters": letters}
+
+
+def develop_dictionary(keys, values):
+    '''
+    develop dictionary from keys and values
+    :param keys:
+    :param values:
+    :return: dictionary of zipped keys and values
+    '''
+    return dict(zip(keys, values))
+
+def main ():
+    code_values = delimit_types(source, 'alpha')
+    code_keys = develop_key_lists()
+
+    numbers_dict = develop_dictionary(code_keys['numbers'], code_values['numerics'])
+    letters_dict = develop_dictionary(code_keys['letters'], code_values['alphabet'])
+    merge(letters_dict, numbers_dict)
+    codes = numbers_dict
+
+    while True:
+        phrase = input("Input a phrase ")
+        if phrase == "exit":
+            break
+        for chr in phrase:
+            print(f"{codes[chr.upper()]} " if chr.upper() in codes.keys() else " ", end='')
+        print('')
+
+if __name__ == '__main__':
+    main()

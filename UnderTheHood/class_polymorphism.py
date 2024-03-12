0d10d8607bd6437e741e3ebe23e7fb429673eacb
phil-qa <104486392+phil-qa@users.noreply.github.com>
2024-03-12 07:13:31 +0000

    Adding polymorphism

---
+++ UnderTheHood/class_polymorphism.py
@@ -0,0 +1,30 @@
+class Car:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Drive!")
+
+class Boat:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Sail!")
+
+class Plane:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Fly!")
+
+car1 = Car("Toyota", "celica")
+boat1 = Boat("Whaler", "speed")
+plane1 = Plane("Boeing", "747")
+
+for x in (car1, boat1, plane1):
+  x.move()
---
+++ UnderTheHood/polymorphism.py
@@ -0,0 +1,14 @@
+'''Functional polymorphism is shown with the len() builtin'''
+
+x = "Phil Bridge"
+
+print(len(x))
+
+mytuple = ("apple", "banana", "cake")
+
+print(len(mytuple))
+
+my_list = ['a small','a large', 'a medium']
+
+print(len(my_list))
+

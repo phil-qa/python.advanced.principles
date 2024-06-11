5d9f336be60c9f78d9d071625a8406f3320629c3
phil-qa <104486392+phil-qa@users.noreply.github.com>
2024-06-11 17:02:17 +0100

    Updating workbooks for new release and adding an additional challenge

--- AdvPy.dg01.additional1.multiples35.py
+++ AdvPy.dg01.additional1.multiples35.py
@@ -1,4 +1,4 @@
-'''If we list all the natural numbers below that are multiples of 3 or 5, we get 3,6, 5, 9.
+'''If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3,6, 5, 9.
 The sum of these multiples is 23
 .
 
